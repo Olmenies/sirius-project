d7ef77d25f8ef38c6a45731d0fcdbfcdab67fa8b
Nicolas Esteban Olmedo <olmedonies@gmail.com>
2022-11-07 17:40:13 -0300

    Made Price card work with Modal

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,13 +1,62 @@
 // Imports
-import { useState } from 'react';
-import { Container, Row, Col, Card, Button, Dropdown } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { Container, Row, Col, Card, Button, Dropdown, Form, Modal } from 'react-bootstrap';
 import { AiFillDollarCircle, AiFillCheckCircle } from 'react-icons/ai';
 
 import './styles.css';
 
 // Component start
 const Services = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
     const [actualStatus, setActualStatus] = useState(''); //Desired status will be saved on actualStatus.value
+    const [actualPrice, setActualPrice] = useState(0);
+    
+
+    const handleOnCloseModal = () => setIsModalVisible(false);
+    const handleOnOpenModal = () => setIsModalVisible(true);
+    const handleOnSubmit = (e, tempPrice) => {
+        setActualPrice(parseInt(tempPrice))
+        handleOnCloseModal();
+    }
+
+    const ServicesPriceModal = ({ value }) => {
+
+        const [tempPrice, setTempPrice] = useState('');
+
+        return (
+            <Modal show={isModalVisible} onHide={handleOnCloseModal}>
+                <Modal.Header>
+                    <Modal.Title>Modificar precio</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Precio actual: ${value}</Form.Label>
+                    </Form.Group>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="controlInput">
+                            <Form.Label>Nuevo precio</Form.Label>
+                            <Form.Control
+                                type="number"
+                                min={0}
+                                placeholder={actualPrice}
+                                autoFocus
+                                onChange={e => setTempPrice(e.target.value)}
+                                value={tempPrice}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleOnCloseModal}>
+                        Cerrar
+                    </Button>
+                    <Button variant="primary" type='button' form='myForm' onClick={(e) => handleOnSubmit(e, tempPrice)}>
+                        Guardar cambios
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
 
     const ServicesStatusDropdown = () => {
 
@@ -38,8 +87,7 @@ const Services = () => {
         );
     }
 
-    const CustomCard = ({ image, title, variable, extra, type }) => {
-        // Conditional styling has to be improved once I'm less burned out
+    const CustomCard = ({ image, title, variable, extra }) => {
         return (
             <Card>
                 <Card.Body>
@@ -48,7 +96,14 @@ const Services = () => {
                         <Card.Text className='my-0'>
                             {title}: {variable}
                         </Card.Text>
-                        {extra}
+                        {extra.map((el, i) => {
+                            return (
+                                {
+                                    ...el,
+                                    key: i
+                                }
+                            );
+                        })}
                     </div>
                 </Card.Body>
             </Card>
@@ -60,20 +115,23 @@ const Services = () => {
             image: <AiFillCheckCircle size={50} />,
             title: 'Estado',
             variable: 'Activo',
-            extra: <ServicesStatusDropdown />,
+            extra: [<ServicesStatusDropdown />],
             type: 'dropdown'
         },
         {
             image: <AiFillDollarCircle size={50} />,
             title: 'Precio',
-            variable: '$69',
-            extra: <Button>Modificar</Button>,
+            variable: actualPrice,
+            extra: [<Button onClick={handleOnOpenModal}>Modificar</Button>, <ServicesPriceModal value={69} />],
             type: 'modal'
         }
     ];
 
     return (
         <Container>
+
+            <ServicesPriceModal />
+
             <Row>
                 <Col className='text-left'>
                     <h2>Servicios</h2>
@@ -90,7 +148,6 @@ const Services = () => {
                                 title={el.title}
                                 variable={el.variable}
                                 extra={el.extra}
-                                type={el.type}
                             />
                         </Col>
                     );
