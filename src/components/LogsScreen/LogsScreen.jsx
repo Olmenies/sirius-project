5435740f30586455c4398c13ec8cfdb828092c58
Nicolas Esteban Olmedo <olmedonies@gmail.com>
2022-11-23 22:22:02 -0300

    Added pagination but broke sort

--- src/components/LogsScreen/LogsScreen.jsx
+++ src/components/LogsScreen/LogsScreen.jsx
@@ -1,6 +1,6 @@
 // Imports
 import { useMemo } from 'react';
-import { useTable, useSortBy } from 'react-table';
+import { useTable, useSortBy, usePagination } from 'react-table';
 
 // Component start
 const LogsScreen = () => {
@@ -10,58 +10,140 @@ const LogsScreen = () => {
             getTableProps,
             getTableBodyProps,
             headerGroups,
-            rows,
+            //rows,
+
+            page, // Instead of using 'rows', we'll use page,
+            // which has only the rows for the active page
+
+            // The rest of these things are super handy, too ;)
+            canPreviousPage,
+            canNextPage,
+            pageOptions,
+            pageCount,
+            gotoPage,
+            nextPage,
+            previousPage,
+            setPageSize,
+            state: { pageIndex, pageSize },
             prepareRow,
         } = useTable(
             {
                 columns,
                 data,
+                initialState: { pageIndex: 2 },
             },
-            useSortBy
+            useSortBy,
+            usePagination
         )
-        const firstPageRows = rows.slice(0, 20)
 
 
         return (
-            <table {...getTableProps()}>
-                <thead>
-                    {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                // Add the sorting props to control sorting. For this example
-                                // we can add them into the header props
-                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                    {column.render('Header')}
-                                    {/* Add a sort direction indicator */}
-                                    <span>
-                                        {column.isSorted
-                                            ? column.isSortedDesc
-                                                ? ' 🔽'
-                                                : ' 🔼'
-                                            : ''}
-                                    </span>
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {firstPageRows.map(
-                        (row, i) => {
-                            prepareRow(row);
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {row.cells.map(cell => {
-                                        return (
-                                            <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                        )
-                                    })}
-                                </tr>
-                            )
-                        }
-                    )}
-                </tbody>
-            </table>
+            <div>
+
+                <pre>
+                    <code>
+                        {JSON.stringify(
+                            {
+                                pageIndex,
+                                pageSize,
+                                pageCount,
+                                canNextPage,
+                                canPreviousPage,
+                            },
+                            null,
+                            2
+                        )}
+                    </code>
+                </pre>
+
+                <table {...getTableProps()}>
+                    <thead>
+                        {headerGroups.map(headerGroup => (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map(column => (
+                                    // Add the sorting props to control sorting. For this example
+                                    // we can add them into the header props
+                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                        {column.render('Header')}
+                                        {/* Add a sort direction indicator */}
+                                        <span>
+                                            {column.isSorted
+                                                ? column.isSortedDesc
+                                                    ? ' 🔽'
+                                                    : ' 🔼'
+                                                : ''}
+                                        </span>
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+                        {page.map(
+                            (row, i) => {
+                                prepareRow(row);
+                                return (
+                                    <tr {...row.getRowProps()}>
+                                        {row.cells.map(cell => {
+                                            return (
+                                                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                            )
+                                        })}
+                                    </tr>
+                                )
+                            }
+                        )}
+                    </tbody>
+                </table>
+
+
+                <div className="pagination">
+                    <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                        {'<<'}
+                    </button>{' '}
+                    <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                        {'<'}
+                    </button>{' '}
+                    <button onClick={() => nextPage()} disabled={!canNextPage}>
+                        {'>'}
+                    </button>{' '}
+                    <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                        {'>>'}
+                    </button>{' '}
+                    <span>
+                        Page{' '}
+                        <strong>
+                            {pageIndex + 1} of {pageOptions.length}
+                        </strong>{' '}
+                    </span>
+                    <span>
+                        | Go to page:{' '}
+                        <input
+                            type="number"
+                            defaultValue={pageIndex + 1}
+                            onChange={e => {
+                                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                                gotoPage(page)
+                            }}
+                            style={{ width: '100px' }}
+                        />
+                    </span>{' '}
+                    <select
+                        value={pageSize}
+                        onChange={e => {
+                            setPageSize(Number(e.target.value))
+                        }}
+                    >
+                        {[10, 20, 30, 40, 50].map(pageSize => (
+                            <option key={pageSize} value={pageSize}>
+                                Show {pageSize}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+
+            </div>
         )
     }
 
@@ -87,16 +169,112 @@ const LogsScreen = () => {
 
     const data = [
         {
-            idCol:'01',
-            msgCol:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
-            tmpCol:'69',
-            excepCol:'Lorem ipsum dolor sit amet.'
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
         },
         {
-            idCol:'01',
-            msgCol:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
-            tmpCol:'69',
-            excepCol:'Lorem ipsum dolor sit amet.'
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            idCol: '01',
+            msgCol: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quas?',
+            tmpCol: '69',
+            excepCol: 'Lorem ipsum dolor sit amet.'
         }
     ]
 
